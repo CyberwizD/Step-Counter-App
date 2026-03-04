6606e3f05f11db8d07cd02b46e051eb8d06a0dce
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2026-03-04 16:53:41 +0100

    feat(profile): add profile screen for user profile

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,26 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { Collapsible } from '@/components/ui/collapsible';
+import { ExternalLink } from '@/components/external-link';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Fonts } from '@/constants/theme';
+
+export default function ProfileScreen() {
+  return (
+    <ThemedView style={styles.profileContainer}>
+      <ThemedText type="title">Hello, welcome back!</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center', 
+  }
+});
